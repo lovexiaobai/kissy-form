eaf83b2145b23b8b17e725eb67f08c900338fecb
daxingplay <daxingplay@gmail.com>
2011-12-13 23:09:17 +0800

    new button

--- uploader/button/base.js
+++ uploader/button/base.js
@@ -1,98 +1,123 @@
 /**
- * @fileoverview 文件上传按钮base
- * @author: 紫英(橘子)<daxingplay@gmail.com>, 剑平（明河）<minghe36@126.com>
- **/
-KISSY.add(function(S, Node, Base) {
-    var EMPTY = '',
-        LOG_PREFIX = '[AjaxUploader-Button] ',
-        $ = Node.all;
-
-    /**
-     * 文件上传按钮
-     * @class Button
-     * @constructor
-     * @param {Object} config 配置对象
-     */
-    function Button(config) {
-        var self = this;
-        /**
-         * 目标容器
-         * @type HTMLElement
-         */
-        self.target = $(config.target);
-        /**
-         * 文件路径隐藏域
-         * @type HTMLElement
-         */
-        self.urlsInput = EMPTY;
-        //超类初始化
-        Button.superclass.constructor.call(self, config);
-    }
-
-    S.mix(Button, {
-        //支持的事件
-        event : { 
-        	'beforeShow': 'beforeShow',
+ * @fileoverview 文件上传按钮
+ * @author 紫英(橘子)<daxingplay@gmail.com>, 剑平(明河)<minghe36@126.com>
+ */
+KISSY.add('upload-btn', function(S, Node, Base){
+	
+	var $ = Node.all,
+		EMPTY = '',
+		LOG_PRE = '[AjaxUploader-Button] ';
+	
+	/**
+	 * 文件上传按钮
+	 * @class Button
+	 * @constructor
+	 * @param {Object} config 配置对象
+	 */
+	function Button(config){
+		var self = this;
+		
+		self.config = S.mix({
+			'type': 'html'
+		}, config);
+		
+		var	module = self.config.type;
+		if(S.inArray(module, ['html', 'flash'])){
+			// self.set('target', self.config.target);
+			// self.target = $(self.config.target);
+			S.use(module + '-button', function(S, Mod){
+				// inst = new Mod(config);
+				// self.set('instance', new Mod(config));
+				self.instance = new Mod(self.config);
+				S.log(LOG_PRE + 'Button loaded. Type: ' + module);
+			});
+			self.render();
+		}else{
+			S.log(LOG_PRE + 'button type does not exists.');
+		}
+	}
+	
+	S.mix(Button, {
+		'event': {
+			'beforeShow': 'beforeShow',
         	'afterShow': 'afterShow',
         	'beforeHide': 'beforeHide',
         	'afterHide': 'afterHide',
-        	'beforeRender' : 'beforeRender', 
-        	'afterRender' : 'afterRender',
-        	'CHANGE' : 'change'
-        }
-    });
-        
-    S.extend(Button, Base, /** @lends Button.prototype*/{
-    	/**
-    	 * 显示按钮
-    	 */
-    	show: function(){
-    		
-    	},
-    	/**
-    	 * 隐藏按钮
-    	 */
-    	hide: function(){
-    		
-    	}
-    },{
-    	ATTRS : /** @lends Button */{
-            /**
-	         * 隐藏的文件路径隐藏域模板
-	         * @type String
-	         */
-	        urlsInputTpl : {
-	            value : '<input type="hidden" value="" name="{name}" class="J_UploaderUrlsInput">'
-	        },
-            /**
-	         * 多个文件时使用的分隔符
-	         * @type String
-	         */
-	        urlDivision : {
-	            value : ','
-	        },
-	        /**
-	         * 是否开启多选支持
-	         * @type Boolean
-	         */
-	        multiple : {
-	            value : false
-	        },
-	        /**
-	         * 是否可用,false为可用
-	         * @type Boolean
-	         */
-	        disabled : {
-	            value : false,
-	        }
-    	}
-    });
-    
-    return Button;
-    
+        	'change' : 'change'
+		}
+	})
+	
+	S.extend(Button, Base, {
+		/**
+		 * 渲染按钮
+		 */
+		render: function(){
+			var self = this,
+				inst = self.instance;
+			inst._render();
+			// self._createUrlsInput();
+		},
+		/**
+		 * 显示按钮
+		 */
+		show: function(){
+			
+		},
+		/**
+		 * 隐藏按钮
+		 */
+		hide: function(){
+			
+		}
+		
+	}, {
+		ATTRS: {
+			/**
+			 * 具体类型的Button实例
+			 * @type Object
+			 */
+			// instance: {
+				// value: null
+			// },
+			/**
+			 * 
+			 */
+			// target: {
+				// value: null,
+				// setter: function(v){
+					// if(S.isString(v)){
+						// return $(v);
+					// }
+				// }
+			// }
+			disabled: {
+				value: false,
+				setter: function(v){
+					var self = this,
+						inst = self.instance;
+					// if(v == false){
+						// self
+					// }
+					// self.instance.disable();
+					S.log(LOG_PRE + '111');
+					self.instance.set('disabled', v);
+					return v;
+				},
+				getter: function(){
+					S.log(LOG_PRE + '1123124')
+				}
+			}
+		}
+	});
+	
+	return Button;
 }, {
-	requires:[
+	requires: [
 		'node',
 		'base'
 	]
 });
+/**
+ * CHANGELOG
+ * 2011-12-13 部分内部属性就不要用set和get了。
+ */
